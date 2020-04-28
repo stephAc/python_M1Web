1a327e8863febe7f46833f7ffce88da7f61c4b6d
Stéphane <stephane.achari@primeradiant.fr>
2020-04-28 22:15:56 +0200

    bash

---
+++ bash/compt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+compt=0
+
+while (($compt < 6))
+do
+	echo $compt
+	compt=$(( compt+1 ))
+done
---
+++ bash/nameUser.sh
@@ -0,0 +1,4 @@
+#!/bine/bash
+echo "Entrer votre nom"
+read name
+echo "Bonjour $name, nous somme le $(date)"
